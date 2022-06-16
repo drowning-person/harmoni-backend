fecbf219782c5ac15896aa0397812efa05c37b56
Flyskea <1696866717@qq.com>
2022-06-16 11:19:47 +0800

    新增router模块

--- main.go
+++ main.go
@@ -1,39 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fiberLearn/apis"
-	"math/rand"
-	"time"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	jwtware "github.com/gofiber/jwt/v3"
+	"fiberLearn/router"
 )
 
 func main() {
-	app := fiber.New()
-	app.Use(logger.New())
-	app.Get("/", func(c *fiber.Ctx) error {
-		data, _ := json.Marshal(map[string]interface{}{
-			"string": "Hello, World!",
-			"number": rand.Intn(100),
-			"date":   time.Now().Format(time.RFC3339),
-		})
-		return c.SendString(string(data))
-	})
-	app.Post("/regist", apis.Regist)
-	app.Post("/login", apis.Login)
-	{
-		appUser := app.Group("/apis/v1")
-		appUser.Use(jwtware.New(jwtware.Config{
-			ErrorHandler: func(c *fiber.Ctx, err error) error {
-				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Error": "请先登录"})
-			},
-			SigningKey: []byte("secret"),
-		}))
-		appUser.Get("/user", apis.GetAllUsers)
-		appUser.Get("/user/:id", apis.GetUser)
-	}
+	app := router.New()
 	app.Listen(":80")
 }
---
+++ router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"fiberLearn/apis"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	jwtware "github.com/gofiber/jwt/v3"
+)
+
+func New() *fiber.App {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Post("/regist", apis.Regist)
+	app.Post("/login", apis.Login)
+
+	appWithAuth := app.Group("/apis/v1", jwtware.New(jwtware.Config{
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Error": "请先登录"})
+		},
+		SigningKey: []byte("secret"),
+	}))
+
+	{
+		appUser := appWithAuth.Group("/user")
+		appUser.Get("", apis.GetAllUsers)
+		appUser.Get(":id", apis.GetUser)
+	}
+	return app
+}
